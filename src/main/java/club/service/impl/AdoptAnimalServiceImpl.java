02328438d87cb430ca8555d96b376a384c3e3c0c
zhangkang <810884285@qq.com>
2020-01-11 20:41:00 +0800

    fourth commit

--- src/main/java/club/service/impl/AdoptAnimalServiceImpl.java
+++ src/main/java/club/service/impl/AdoptAnimalServiceImpl.java
@@ -3,9 +3,13 @@ package club.service.impl;
 import club.dao.AdoptAnimalMapper;
 import club.pojo.AdoptAnimal;
 import club.service.AdoptAnimalService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+@Service
+@Transactional
 public class AdoptAnimalServiceImpl implements AdoptAnimalService {
 
     @Resource
